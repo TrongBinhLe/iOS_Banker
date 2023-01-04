48361cf164e06451a557cae35acc7e61b6c5b4a5
TrongBinhLe <ltbinhbk2010@gmail.com>
2023-01-04 10:00:36 +0700

    Adding the error message label

--- LoginViewController.swift
+++ LoginViewController.swift
@@ -11,6 +11,7 @@ class LoginViewController: UIViewController {
 
     let loginView = LoginView()
     let signInButton = UIButton(type: .system)
+    let errorMessageLabel = UILabel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,13 +30,19 @@ extension LoginViewController {
         signInButton.setTitle("Sign In", for: .normal)
         signInButton.addTarget(self, action: #selector(signInTapped), for: .primaryActionTriggered)
         
-        
+        errorMessageLabel.translatesAutoresizingMaskIntoConstraints = false
+        errorMessageLabel.textAlignment = .center
+        errorMessageLabel.textColor = .systemRed
+        errorMessageLabel.numberOfLines = 0
+        errorMessageLabel.text = "Username / password cannot be blank"
+        errorMessageLabel.isHidden = false
     }
     
     private func layout() {
     
         view.addSubview(loginView)
         view.addSubview(signInButton)
+        view.addSubview(errorMessageLabel)
         
         // Layout LoginView
         NSLayoutConstraint.activate([
@@ -49,6 +56,13 @@ extension LoginViewController {
             signInButton.topAnchor.constraint(equalToSystemSpacingBelow: loginView.bottomAnchor, multiplier: 2),
             signInButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             signInButton.leadingAnchor.constraint(equalTo: loginView.leadingAnchor),
+        ])
+        
+        // Layout ErrorMessage
+        NSLayoutConstraint.activate([
+            errorMessageLabel.topAnchor.constraint(equalToSystemSpacingBelow: signInButton.bottomAnchor, multiplier: 2),
+            errorMessageLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            errorMessageLabel.leadingAnchor.constraint(equalTo: signInButton.leadingAnchor),
         ])
     }
 }
