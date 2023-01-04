2b4306d177945f2bb80ad646ab92dff3ce9957a5
TrongBinhLe <ltbinhbk2010@gmail.com>
2023-01-04 09:29:55 +0700

    Adding the sign-in button without autolayout

--- LoginViewController.swift
+++ LoginViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class LoginViewController: UIViewController {
 
     let loginView = LoginView()
+    let signInButton = UIButton(type: .system)
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,15 +22,40 @@ class LoginViewController: UIViewController {
 extension LoginViewController {
     private func style() {
         loginView.translatesAutoresizingMaskIntoConstraints = false
+        
+        signInButton.translatesAutoresizingMaskIntoConstraints = false
+        signInButton.configuration = .filled()
+        signInButton.configuration?.imagePadding = 8
+        signInButton.setTitle("Sign In", for: .normal)
+        signInButton.addTarget(self, action: #selector(signInTapped), for: .primaryActionTriggered)
+        
+        
     }
     
     private func layout() {
     
         view.addSubview(loginView)
+        view.addSubview(signInButton)
+        
+        // Layout LoginView
         NSLayoutConstraint.activate([
             loginView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
             loginView.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 1),
-            view.trailingAnchor.constraint(equalToSystemSpacingAfter: loginView.trailingAnchor, multiplier: 1)
+            view.trailingAnchor.constraint(equalToSystemSpacingAfter: loginView.trailingAnchor, multiplier: 1),
         ])
+        
+        // Layout SignInButton
+        NSLayoutConstraint.activate([
+            signInButton.topAnchor.constraint(equalToSystemSpacingBelow: loginView.bottomAnchor, multiplier: 2),
+            signInButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            signInButton.leadingAnchor.constraint(equalTo: loginView.leadingAnchor),
+        ])
+    }
+}
+
+extension LoginViewController {
+    
+    @objc private func signInTapped() {
+        
     }
 }
